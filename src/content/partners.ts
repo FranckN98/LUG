b4b09fc62794291d162bd087fef1341a3624d14f
FranckN98 <ufrank238@yahoo.fr>
2026-08-11 23:17:09 +0200

    Add Cave des Montagnes partner logo

--- src/content/partners.ts
+++ src/content/partners.ts
@@ -8,6 +8,7 @@ export const partners2025: Partner[] = [
 
   { name: 'Regus', logo: '/partners/regus.png', website: 'https://www.regus.com/de' },
   { name: 'Campusdirekt', logo: '/partners/campusdirekt.webp', website: 'https://campusdirekt.de/' },
+  { name: 'Cave des Montagnes', logo: '/partners/cave-des-montagnes.png', website: 'https://cave-des-montagnes.com/de/' },
   { name: 'AfroGeek', logo: '/partners/5.png' },
   { name: 'GrowInDE', logo: '/partners/4.png' },
   { name: 'SB Salon', logo: '/partners/10.png' },
